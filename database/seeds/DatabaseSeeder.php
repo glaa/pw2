1fc8c0fa231225b09240d2c6a57a2118fc16ab93
Fagner Barros <fagnerluizbarros@gmail.com>
2019-10-02 00:25:13 -0300

    Model, Migrate, Factory e Seeder de  Endereco. Faker em pt-BR

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(UsuarioTableSeeder::class);
+        $this->call(ClienteTableSeeder::class);
+        $this->call(EnderecoTableSeeder::class);
     }
 }

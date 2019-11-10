28651f0f80dfa015bc19a513b26a8656885bf010
Gleison A. Araujo <gleisonaaraujo@gmail.com>
2019-11-10 17:16:58 -0300

    Realizado o relacionamento de Agenda, Agendamento, Atendimento e Cliente

--- app/Agenda.php
+++ app/Agenda.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Agenda extends Model
 {
     //
+    public function agendamentos()
+    {
+        return $this->hasMany('App\Agendamento');
+    }
+
+    public function estabelecimento()
+    {
+        return $this->belongsTo('App\Estabelecimento');
+    }
+
+    public function profissional()
+    {
+        return $this->belongsToMany('App\Profissional');
+    }
 }
